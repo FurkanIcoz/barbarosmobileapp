9e40702fc18e249a6f8ce09befba0c8996970144
FURKAN İÇÖZ <furkanicoz@outlook.com>
2024-07-30 19:52:58 +0300

    Logn

--- src/screens/LoginPage.js
+++ src/screens/LoginPage.js
@@ -1,16 +1,39 @@
 import React from "react";
-import { Pressable, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 const LoginPage = ({navigation})=>{
     return(
-        <View>
+        <View style={styles.container}>
             <Pressable 
                 onPress={()=> navigation.navigate('Register')}
+                style={styles.loginButton}
             >
-                <Text> Sign Up</Text>
+                <Text style={styles.buttonText}> Sign Up</Text>
             </Pressable>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:"center",
+        justifyContent:"center"
+    },
+    loginButton:{
+        backgroundColor:'lightblue',
+        borderRadius:20,
+        width: '80%',
+        alignItems:"center",
+        height:50,
+        justifyContent:"center"
+    },
+    buttonText:{
+        fontWeight:"bold",
+        fontSize: 25,
+
+        
+    }
+})
 
 export default LoginPage;
